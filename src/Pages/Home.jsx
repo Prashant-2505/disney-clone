8d811f38912727fb58debccf45ac5eb414c48823
Prashant-2505 <shramaprashant2505@gmail.com>
2023-03-16 20:33:58 +0530

    add background image in home section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import backgroundImg from '../images/home-background.png';
 
 const Home = () => {
-
-
   return (
-    <div>
-      <h1>This is home</h1>
+    <div style={{ backgroundImage: `url(${backgroundImg})`, height: '100vh', width: '100vw' }}>
+      {/* Your content here */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
